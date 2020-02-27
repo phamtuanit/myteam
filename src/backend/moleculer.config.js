b2d2aee8d13920e26965ba1b23df0a447e6e8bb8
tuanpham <tuanpham@gcs-vn.com>
2020-02-27 17:24:07 +0700

    Update config

--- src/backend/moleculer.config.js
+++ src/backend/moleculer.config.js
@@ -29,7 +29,7 @@ const sysConf = require("./conf/system.json");
  */
 module.exports = {
 	// Namespace of nodes to segment your nodes on the same network.
-	namespace: "",
+	namespace: "myteams",
 	// Unique node identifier. Must be unique in a namespace.
 	nodeID: null,
 	// Custom metadata store. Store here what you want. Accessing: `this.broker.metadata`
@@ -158,7 +158,7 @@ module.exports = {
 
 	// Enable/disable built-in metrics function. More info: https://moleculer.services/docs/0.14/metrics.html
 	metrics: {
-		enabled: true,
+		enabled: false,
 		// Available built-in reporters: "Console", "CSV", "Event", "Prometheus", "Datadog", "StatsD"
 		reporter: {
 			type: "Prometheus",
