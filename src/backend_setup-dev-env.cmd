34e39540949052ac75a9177278321bccdbf5acaf
tuanpham <tuanpham@gcs-vn.com>
2020-03-06 17:51:06 +0700

    Add env comd

---
+++ src/backend_setup-dev-env.cmd
@@ -0,0 +1,14 @@
+cd /d %~dp0
+
+SET CURRPATH=%~dp0
+SET DB_PATH=%CURRPATH%DB
+SET MONGO_DB_PATH=%DB_PATH%\mongodb
+SET REDIS_DB_PATH=%DB_PATH%\redis
+SET REDIS_DB_CONF=%REDIS_DB_PATH%\redis.conf
+
+echo Setting up mongodDB: %DB_PATH%
+start "myteam-mongodb" mongod.exe --dbpath=%MONGO_DB_PATH%  --bind_ip_all
+
+
+echo Setting up Redis: %REDIS_DB_PATH%
+start "myteam-redis" redis-server.exe %REDIS_DB_CONF%
