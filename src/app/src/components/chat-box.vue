c647af138df7a20ceb9282b528a25c3090ab5ae6
tuanpham <tuanpham@gcs-vn.com>
2020-03-14 13:41:31 +0700

    Add Emoji button

--- src/app/src/components/chat-box.vue
+++ src/app/src/components/chat-box.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fill-height">
+  <div class="chat-box fill-height">
     <v-sheet class="overflow-y-auto message-container">
       <v-container style="height: 1500px;" class="pa-0"> </v-container>
     </v-sheet>
@@ -17,35 +17,10 @@
           @keyup.enter="sendMessage"
           clear-icon="mdi-close"
         ></v-text-field>
-        <v-btn icon>
+        <v-btn icon class="send-btn">
           <v-icon>mdi-send</v-icon>
         </v-btn>
-        <v-menu
-          top
-          offset-y
-          v-model="showEmojiPicker"
-          :eager="true"
-          internal-activator
-          transition="scroll-y-transition"
-          :close-on-content-click="false"
-          @keyup.enter="showEmojiPicker = false"
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn icon v-on="on">
-              <v-icon>mdi-emoticon-happy-outline</v-icon>
-            </v-btn>
-          </template>
-
-          <picker
-            :data="emojiData"
-            :exclude="['flags', 'symbols']"
-            :color="theme.dark ? '#1E1E1E' : '#ae65c5'"
-            :showPreview="false"
-            set="twitter"
-            :infiniteScroll="true"
-            @select="onSelectEmoji"
-          ></picker>
-        </v-menu>
+        <EmojiButton></EmojiButton>
       </v-list-item>
     </v-list>
   </div>
@@ -53,16 +28,12 @@
 
 <script>
 import { fillHeight } from "../utils/layout.js";
-import { Picker, EmojiIndex } from "emoji-mart-vue-fast";
-import emojiData from "emoji-mart-vue-fast/data/twitter.json";
-const emojiIndex = new EmojiIndex(emojiData);
+import EmojiButton from "./emoji-button";
 export default {
-  components: { Picker },
+  components: { EmojiButton },
   data() {
     return {
-      emojiData: emojiIndex,
       theme: this.$vuetify.theme,
-      showEmojiPicker: false,
       message: ""
     };
   },
@@ -77,14 +48,14 @@ export default {
       this.message = "";
     },
     onSelectEmoji(emoji) {
-      this.message += emoji.native;
+      this.$emit("select", emoji);
     }
   }
 };
 </script>
 
-<style lang="css">
-.emoji-type-image.emoji-set-twitter {
-  background-image: url("../assets/emoji/twitter-emoji-32.png");
+<style lang="css" scoped>
+.chat-box >>> .theme--light.v-btn.v-btn--icon {
+  color: var(--primary-color-2);
 }
 </style>
---
+++ src/app/src/components/emoji-button.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-menu
+    top
+    offset-y
+    v-model="showEmojiPicker"
+    :eager="true"
+    internal-activator
+    transition="scroll-y-transition"
+    :close-on-content-click="false"
+    @keyup.enter="showEmojiPicker = false"
+  >
+    <template v-slot:activator="{ on }">
+      <v-btn icon v-on="on">
+        <v-icon>mdi-emoticon-happy-outline</v-icon>
+      </v-btn>
+    </template>
+
+    <picker
+      :data="emojiData"
+      color="#043752"
+      :showPreview="false"
+      set="twitter"
+      :infiniteScroll="true"
+      @select="onSelectEmoji"
+    ></picker>
+  </v-menu>
+</template>
+
+<script>
+import { Picker, EmojiIndex } from "emoji-mart-vue-fast";
+import emojiData from "emoji-mart-vue-fast/data/twitter.json";
+import "emoji-mart-vue-fast/css/emoji-mart.css";
+const emojiIndex = new EmojiIndex(emojiData, {
+  exclude: ["flags", "symbols"]
+});
+export default {
+  components: { Picker },
+  data() {
+    return {
+      emojiData: emojiIndex,
+      showEmojiPicker: false
+    };
+  },
+  methods: {
+    onSelectEmoji(emoji) {
+      this.$emit("select", emoji);
+    }
+  }
+};
+</script>
+
+
+<style lang="css">
+.emoji-type-image.emoji-set-twitter {
+  background-image: url("../assets/emoji/twitter-emoji-32.png");
+}
+</style>
