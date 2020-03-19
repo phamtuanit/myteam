9baf7215fb20caba947060d0371d599d58f6c01b
tuanpham <tuanpham@gcs-vn.com>
2020-03-19 11:01:21 +0700

    Improve live event

--- src/backend/mixins/socket.mixin.js
+++ src/backend/mixins/socket.mixin.js
@@ -18,19 +18,24 @@ module.exports = {
         // [NodeID].user.status
         "*.user.status"(data) {
             const userId = data.user ? data.user.id : undefined;
+            const status = data.status;
 
             if (typeof userId == "string") {
                 this.logger.debug(
                     `Status of user ${userId} has been change.`,
                     data
                 );
+
+                const socketDict = this.sockets[userId];
                 // Broadcast to all user
-                Object.keys(this.sockets).forEach(key => {
-                    if (userId != key) {
-                        const socket = this.sockets[key];
-                        socket.send("user.status", data);
+                if (status == "on") {
+                    if (Object.keys(socketDict).length == 1) {
+                        const socket = Object.values(socketDict)[0];
+                        socket.to("live").emit("live", status, data);
                     }
-                });
+                } else if (!socketDict || Object.keys(socketDict).length <= 0) {
+                    this.io.to("live").emit("live", status, data);
+                }
             }
         }
     },
@@ -53,14 +58,40 @@ module.exports = {
         },
         handleNewSocket(socket, user) {
             socket.handshake.user = user;
-            this.sockets[user.id] = socket;
+            const requiredRooms = [user.id, "live"];
+            // Save socket
+            this.sockets[user.id] = this.sockets[user.id] || {};
+            this.sockets[user.id][socket.id] = socket;
             this.logger.info(`User ${user.id} has been connected.`);
 
+            // Join to required room
+            requiredRooms.forEach(room => {
+                socket.join(room);
+            });
+
+            // Broadcast to the others about new user
             const connectedEvt = `${this.broker.nodeID}.user.connected`;
             this.broker.emit(connectedEvt, user, ["live"]); // live service only
 
             socket.on("disconnect", () => {
                 this.logger.info(`User ${user.id} has been disconnected.`);
+
+                // Leave to required room
+                requiredRooms.forEach(room => {
+                    socket.leave(room);
+                });
+
+                // Clear socket storage
+                const socketDict = this.sockets[user.id];
+                const existingSocket = socketDict[socket.id];
+                if (existingSocket) {
+                    delete socketDict[socket.id];
+                }
+
+                if (Object.keys(socketDict).length <= 0) {
+                    delete this.sockets[user.id];
+                }
+
                 const disconnectedEvt = `${this.broker.nodeID}.user.disconnected`;
                 this.broker.emit(disconnectedEvt, user, ["live"]);
             });
@@ -75,23 +106,14 @@ module.exports = {
         },
         onReceivedMessage(channel, message) {
             const [resource, objectId, action] = channel.split(".");
-            const data = {
-                channel,
-                payload: message
-            };
-            switch (resource) {
-                case "message":
-                    {
-                        const socket = this.sockets[objectId];
-                        if (socket) {
-                            socket.send(action, data);
-                        }
-                    }
-                    break;
-
-                default:
-                    this.io.to("live").emit("live", action, data);
-                    break;
+            const socketDict = this.sockets[objectId];
+            if (socketDict && Object.keys(socketDict).length > 0) {
+                const data = {
+                    channel,
+                    payload: message
+                };
+                // To private user room
+                this.io.to(objectId).emit(resource, action, data);
             }
         }
     },
--- src/backend/services/live.service.js
+++ src/backend/services/live.service.js
@@ -17,7 +17,7 @@ module.exports = {
             handler() {
                 return Object.values(this.livingUser).map(user => {
                     return {
-                        user,
+                        info: user,
                         status: "on"
                     };
                 });
@@ -35,7 +35,7 @@ module.exports = {
                 const user = this.livingUser[userId];
                 const status = user ? "on" : "off";
                 return {
-                    user: user || { id: userId },
+                    info: user,
                     status: status
                 };
             }
@@ -49,25 +49,35 @@ module.exports = {
         // [NodeID].user.connected
         "*.user.connected"(user) {
             this.logger.info(`User ${user.id} has been connected.`);
-            this.livingUser[user.id] = user;
-
-            // Inform to 
-            const eventName = `${this.nodeID}.user.status`;
-            this.broker.emit(eventName, {
+            this.livingUser[user.id] = this.livingUser[user.id] || {
                 user,
-                status: "on"
-            });
+                count: 0
+            };
+            this.livingUser[user.id].count += 1;
+
+            if (this.livingUser[user.id].count == 1) {
+                // Inform to
+                const eventName = `${this.broker.nodeID}.user.status`;
+                this.broker.emit(eventName, {
+                    user,
+                    status: "on"
+                });
+            }
         },
         // [NodeID].user.disconnected
         "*.user.disconnected"(user) {
             this.logger.info(`User ${user.id} has been disconnected.`);
-            delete this.livingUser[user.id];
-
-            const eventName = `${this.nodeID}.user.status`;
-            this.broker.emit(eventName, {
-                user,
-                status: "off"
-            });
+            if (this.livingUser[user.id]) {
+                this.livingUser[user.id].count -= 1;
+                if (this.livingUser[user.id].count == 0) {
+                    delete this.livingUser[user.id];
+                    const eventName = `${this.broker.nodeID}.user.status`;
+                    this.broker.emit(eventName, {
+                        user,
+                        status: "off"
+                    });
+                }
+            }
         }
     },
 
