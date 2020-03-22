8bd70657bff88395d99c51159ab52ad6fec6d333
tuanpham <tuanppham@gcs-vn.com>
2020-03-22 11:27:02 +0700

    Impl searching user

--- src/app/src/pages/chat/chat-list.vue
+++ src/app/src/pages/chat/chat-list.vue
@@ -6,7 +6,11 @@
     id="chat-list"
   >
     <!-- Search -->
-    <v-list dense dark class="pb-0">
+    <v-list
+      dense
+      dark
+      class="pb-0"
+    >
       <v-list-item class="px-3 mt-1">
         <v-text-field
           v-model="searchText"
@@ -19,48 +23,52 @@
           clearable
           clear-icon="mdi-close"
         ></v-text-field>
-        <v-btn icon size="40" outlined class="ml-2" @click="onAddChat">
+        <v-btn
+          icon
+          size="40"
+          outlined
+          class="ml-2"
+          @click="onAddChat"
+        >
           <v-icon v-text="plusIcon"></v-icon>
         </v-btn>
       </v-list-item>
     </v-list>
 
     <!-- List -->
-    <v-list two-line dark class="py-0 px-0">
-      <v-subheader class="pl-3 pr-2" v-text="title"></v-subheader>
+    <v-list
+      two-line
+      dark
+      class="py-0 px-0"
+    >
+      <v-subheader
+        class="pl-3 pr-2"
+        v-text="title"
+      ></v-subheader>
       <v-progress-linear
         :active="loading"
         :indeterminate="true"
       ></v-progress-linear>
 
       <v-list-item-group v-model="selectedConv">
-        <!-- 1 -->
-        <v-list-item @click="openChat()">
+        <v-list-item
+          v-for="conv in conversations"
+          :key="conv.user.id"
+          @click="openChat()"
+        >
           <v-avatar size="30">
             <v-img src="https://randomuser.me/api/portraits/men/81.jpg"></v-img>
           </v-avatar>
 
           <v-list-item-content class="py-2 px-2">
-            <v-list-item-title class="body-2">Jon Son</v-list-item-title>
-            <v-list-item-subtitle class="caption"
-              >Can I help you? It seems that you facce a big
-              problem.</v-list-item-subtitle
-            >
-          </v-list-item-content>
-        </v-list-item>
-
-        <!-- 2 -->
-        <v-list-item @click="openChat()">
-          <v-avatar size="30">
-            <v-img src="https://randomuser.me/api/portraits/men/41.jpg"></v-img>
-          </v-avatar>
-
-          <v-list-item-content class="py-2 px-2">
-            <v-list-item-title class="body-2">Wilance Joy</v-list-item-title>
-            <v-list-item-subtitle class="caption"
-              >Can I help you? It seems that you facce a big
-              problem.</v-list-item-subtitle
-            >
+            <v-list-item-title
+              class="body-2"
+              v-text="conv.name"
+            ></v-list-item-title>
+            <v-list-item-subtitle
+              class="caption"
+              v-text="conv.additional"
+            ></v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
@@ -71,79 +79,102 @@
 <script>
 // import { fillHeight } from "../utils/layout.js";
 export default {
-  data() {
-    return {
-      loading: false,
-      selectedConv: null,
-      searchText: null,
-      displayMode: "chat"
-    };
-  },
-  computed: {
-    title() {
-      return this.displayMode == "chat" ? "Conversations" : "Friends";
+    data() {
+        return {
+            loading: false,
+            selectedConv: null,
+            searchText: null,
+            displayMode: "chat",
+            conversations: [],
+        };
     },
-    plusIcon() {
-      return "mdi-" + (this.displayMode == "chat" ? "plus" : "close");
-    }
-  },
-  mounted() {
-    // fillHeight(this.$el, 0);
-  },
-  methods: {
-    openChat() {},
-    onAddChat() {
-      // Reset search
-      this.searchText = "";
+    computed: {
+        title() {
+            return this.displayMode == "chat" ? "Conversations" : "Friends";
+        },
+        plusIcon() {
+            return "mdi-" + (this.displayMode == "chat" ? "plus" : "close");
+        },
+    },
+    watch: {
+        searchText(val) {
+            if (this.displayMode == "friend") {
+                if (val) {
+                    this.searchLocker.then(this.searchFriend);
+                }
+            }
+        },
+    },
+    mounted() {
+        this.searchLocker = Promise.resolve();
+    },
+    methods: {
+        openChat() {},
+        onAddChat() {
+            // Reset search
+            this.searchText = "";
 
-      if (this.displayMode == "chat") {
-        this.displayMode = "friend";
-        // Backup data
-        this.conversationsBackup = this.conversations;
-        this.loading = true;
-        this.loadFriendList()
-          .then(list => {
-            this.conversations = list || [];
-          })
-          .finally(() => {
-            this.loading = false;
-          });
-      } else {
-        this.displayMode = "chat";
-        // Restore data
-        this.conversations = this.conversationsBackup;
-      }
+            if (this.displayMode == "chat") {
+                // Backup data
+                this.conversationsBackup = this.conversations;
+                this.displayMode = "friend";
+            } else {
+                this.displayMode = "chat";
+                // Restore data
+                this.conversations = this.conversationsBackup;
+            }
+        },
+        searchFriend() {
+            if (!this.searchText) {
+                this.conversations = [];
+                this.searchLocker = Promise.resolve();
+                return;
+            }
+
+            this.loading = true;
+            this.searchLocker = this.$store
+                .dispatch("users/findUser", this.searchText)
+                .then(users => {
+                    this.conversations = [];
+                    if (users) {
+                        users.forEach(user => {
+                            this.conversations.push({
+                                name: user.firstname + ", " + user.lastname,
+                                user: user,
+                                additional: user.status,
+                            });
+                        });
+                    }
+                })
+                .catch(console.error)
+                .finally(() => {
+                    this.loading = false;
+                    this.searchLocker = Promise.resolve();
+                });
+        },
     },
-    loadFriendList() {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve([]);
-        }, 2 * 1000);
-      });
-    }
-  }
 };
 </script>
 
 <style scoped>
 #chat-list,
 .v-list {
-  background-color: var(--primary-color-2) !important;
+    background-color: var(--primary-color-2) !important;
 }
 
 #chat-list >>> .v-text-field.v-text-field--solo .v-input__control {
-  min-height: 40px !important;
+    min-height: 40px !important;
 }
 
 #chat-list >>> .v-text-field--rounded > .v-input__control > .v-input__slot {
-  padding: 0 16px;
+    padding: 0 16px;
 }
 
 #chat-list >>> .v-text-field--rounded {
-  border-radius: 20px;
+    border-radius: 20px;
 }
 
 #chat-list >>> .v-btn {
-  border-color: rgba(255, 255, 255, 0.16);
+    border-color: rgba(255, 255, 255, 0.16);
 }
 </style>
