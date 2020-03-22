64995a7fdf4dce19dca0b764bed79c4e1818e100
tuanpham <tuanppham@gcs-vn.com>
2020-03-22 23:11:39 +0700

    Fix changing dark mode

--- src/app/src/components/left-drawer.vue
+++ src/app/src/components/left-drawer.vue
@@ -1,61 +1,65 @@
 <template>
-    <v-navigation-drawer
-        permanent
-        mini-variant
-        dark
-        app
-        mini-variant-width="70"
-        class="left-drawer"
-    >
-        <v-list-item class="px-0 mt-2 mb-1 mx-auto">
-            <v-avatar size="40" class="mx-auto">
-                <v-img
-                    src="https://randomuser.me/api/portraits/men/85.jpg"
-                ></v-img>
-            </v-avatar>
-        </v-list-item>
-        <v-list>
-            <v-list-item-group mandatory>
-                <template v-for="menu in menus">
-                    <v-list-item
-                        :key="menu.key"
-                        :value="menu"
-                        :input-value="menu == activatedMenu"
-                        class="px-0 my-0 pb-3 pt-4"
-                        @click="onActivateMenu(menu)"
-                    >
-                        <v-badge
-                            :value="menu.inform.count > 0"
-                            :key="menu.key + 'badge'"
-                            overlap
-                            color="red darken-3"
-                            light
-                            :content="menu.inform.count"
-                        >
-                            <v-tooltip right>
-                                <template v-slot:activator="{ on }">
-                                    <v-icon
-                                        color="white"
-                                        v-on="on"
-                                        class="mx-auto pa-1"
-                                        >mdi-{{ menu.icon }}</v-icon
-                                    >
-                                </template>
-                                <span>{{ menu.name }}</span>
-                            </v-tooltip>
-                        </v-badge>
-                    </v-list-item>
+  <v-navigation-drawer
+    permanent
+    mini-variant
+    dark
+    app
+    mini-variant-width="70"
+    class="left-drawer"
+  >
+    <v-list-item class="px-0 mt-2 mb-1 mx-auto">
+      <v-avatar
+        size="40"
+        class="mx-auto"
+      >
+        <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+      </v-avatar>
+    </v-list-item>
+    <v-list>
+      <v-list-item-group mandatory>
+        <template v-for="menu in menus">
+          <v-list-item
+            :key="menu.key"
+            :value="menu"
+            :input-value="menu == activatedMenu"
+            class="px-0 my-0 pb-3 pt-4"
+            @click="onActivateMenu(menu)"
+          >
+            <v-badge
+              :value="menu.inform.count > 0"
+              :key="menu.key + 'badge'"
+              overlap
+              color="red darken-3"
+              light
+              :content="menu.inform.count"
+            >
+              <v-tooltip right>
+                <template v-slot:activator="{ on }">
+                  <v-icon
+                    color="white"
+                    v-on="on"
+                    class="mx-auto pa-1"
+                  >mdi-{{ menu.icon }}</v-icon>
                 </template>
-            </v-list-item-group>
-        </v-list>
-        <template v-slot:append>
-            <div class="pa-2 text-center">
-                <v-btn icon @click="enableDarkMode" title="Dark / Light">
-                    <v-icon>mdi-theme-light-dark</v-icon>
-                </v-btn>
-            </div>
+                <span>{{ menu.name }}</span>
+              </v-tooltip>
+            </v-badge>
+          </v-list-item>
         </template>
-    </v-navigation-drawer>
+      </v-list-item-group>
+    </v-list>
+    <template v-slot:append>
+      <div class="pa-2 text-center">
+        <v-btn
+          icon
+          @click="enableDarkMode"
+          title="Dark / Light"
+        >
+          <v-icon>mdi-theme-light-dark</v-icon>
+        </v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
 </template>
 
 <script>
@@ -74,11 +78,14 @@ export default {
     methods: {
         enableDarkMode() {
             try {
-                const theme = { ...this.theme };
+                const theme = this.theme;
                 theme.dark = !theme.dark;
-                this.$store.dispatch("setTheme", theme);
-
-                this.$vuetify.theme.dark = theme.dark;
+                this.$store
+                    .dispatch("setTheme", theme)
+                    .then(() => {
+                        this.$vuetify.theme.dark = theme.dark;
+                    })
+                    .catch(console.error);
             } catch (error) {
                 console.error("Could not change Dark mode", error);
             }
