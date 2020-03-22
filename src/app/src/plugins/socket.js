860e5f33d1d3d0353837206d27555daa2b15899e
tuanpham <tuanppham@gcs-vn.com>
2020-03-22 11:27:12 +0700

    Restructure

---
+++ src/app/src/plugins/socket.js
@@ -0,0 +1,112 @@
+const socket = require('socket.io-client');
+const authSvr = window.IoC.get('auth');
+
+/**
+ * Socket class
+ */
+class Socket {
+    /**
+     * Constructor
+     * @param {*} path 
+     */
+    constructor(baseUri, path, retryTimes = 10) {
+        this.baseUri = baseUri;
+        this.path = path;
+        this.retryTimes = retryTimes;
+        this.io = null;
+        this.subscribers = {};
+        this.rooms = [];
+
+        this.eventBus = window.IoC.get('bus');
+        this.eventBus.on("logout", () => this.close());
+        this.eventBus.on("login", () => this.connect());
+    }
+    /**
+     * Get socket status
+     *
+     * @memberof Socket
+     */
+    getStatus() {
+        return this.io != null && typeof this.io == "object";
+    }
+    /**
+     * Register listener
+     * @param {string} evt event name
+     * @param {function} callback callback function
+     */
+    on(evt, callback) {
+        if (evt && callback) {
+            this.subscribers[evt] = this.subscribers[evt] || [];
+            this.subscribers[evt].push(callback);
+
+            if (this.io) {
+                // WS is connected
+                this.io.on(evt, callback);
+            }
+        }
+        return this;
+    }
+    /**
+     * Close Socket
+     */
+    close() {
+        if (this.io) {
+            console.info("Close Socket connection.");
+            this.io.close();
+        }
+        this.io = undefined;
+        return this;
+    }
+    /**
+     * Connect to server with token
+     */
+    connect() {
+        this.close();
+        return authSvr.getToken().then(token => {
+            this.io = socket(this.baseUri, {
+                path: this.path,
+                query: {
+                    token: token
+                }
+            });
+    
+            this.registerEvents();
+            this.io.connect();
+            return this;
+        });
+    }
+    /**
+     * Register subscriber to inner socket
+     */
+    registerEvents() {
+        // Register message handler
+        Object.keys(this.subscribers).forEach(evt => {
+            const callbackList = this.subscribers[evt];
+            callbackList.forEach(callback => {
+                this.io.on(evt, callback);
+            });
+        });
+
+        this.io.on('connect', () => {
+            console.info("Socket connection is available now");
+            // Join to given room
+            this.rooms.forEach(room => {
+                this.io.emit('join', room);
+            });
+        });
+
+        this.io.on('connect_error', (error) => {
+            console.error("Socket connection is interrupted.", error);
+        });
+
+        this.io.on('disconnect', (reason) => {
+            console.error("Socket connection is disconnected.", reason);
+            if (reason === 'io server disconnect') {
+                // The disconnection was initiated by the server, reconnect manually
+                this.io.connect();
+            }
+        });
+    }
+}
+
+module.exports = Socket;
