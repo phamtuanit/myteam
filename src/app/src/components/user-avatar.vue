9b3deda23992982859841aee2233f4833ef4d1c5
tuanpham <tuanpham@gcs-vn.com>
2020-03-23 14:43:09 +0700

    Implement user-avatar

---
+++ src/app/src/components/user-avatar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-badge
+        :color="isOnline ? 'success' : 'pink'"
+        dot light
+        bottom
+        overlap
+        bordered
+    >
+        <v-avatar size="30">
+            <v-img src="https://randomuser.me/api/portraits/men/81.jpg"></v-img>
+        </v-avatar>
+    </v-badge>
+</template>
+
+<script>
+export default {
+    props: ["user"],
+    computed: {
+        isOnline() {
+            return this.user.status == "on";
+        },
+    },
+};
+</script>
+
+<style>
+</style>
