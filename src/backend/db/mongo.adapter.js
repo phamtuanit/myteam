00a0ca3a23ad92812916c0e3391da73e85fc71c1
tuanpham <tuanpham@gcs-vn.com>
2020-03-25 17:40:15 +0700

    Support update with filter

--- src/backend/db/mongo.adapter.js
+++ src/backend/db/mongo.adapter.js
@@ -1,6 +1,25 @@
 const dbConf = require("../conf/db.json");
 const MongoAdapter = require("moleculer-db-adapter-mongo");
 
+class MongoAdapterExt extends MongoAdapter {
+    constructor(uri, opts, dbName) {
+        super(uri, opts, dbName);
+    }
+
+	/**
+	 * Update an entity by filter
+	 *
+	 * @param {String} filter - filter object
+	 * @param {Object} update
+	 * @returns {Promise<Object>} Return with the updated document.
+	 *
+	 * @memberof MongoDbAdapterExt
+	 */
+    update(filter, update) {
+		return this.collection.findOneAndUpdate(filter, update, { returnOriginal : false }).then(res => res.value);
+	}
+}
+
 module.exports = function(collection, service) {
     // Mongo adapter
     const uri = process.env.MONGO_URI || dbConf.mongodb.uri;
@@ -11,7 +30,7 @@ module.exports = function(collection, service) {
         logger: service.logger,
     };
 
-    const adapter = new MongoAdapter(
+    const adapter = new MongoAdapterExt(
         uri,
         { useUnifiedTopology: true },
         dbConf.mongodb.dbName
