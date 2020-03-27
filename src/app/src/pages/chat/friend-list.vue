e82517e401408f3def12ad22f3164cfedd4af4c0
tuanpham <tuanpham@gcs-vn.com>
2020-03-27 09:46:41 +0700

    Update layout

--- src/app/src/pages/chat/friend-list.vue
+++ src/app/src/pages/chat/friend-list.vue
@@ -18,16 +18,6 @@
                     clearable
                     clear-icon="mdi-close"
                 ></v-text-field>
-
-                <v-btn
-                    icon
-                    class="ml-2"
-                    outlined
-                    @click="onAddChat"
-                    :disabled="!selectedUser"
-                >
-                    <v-icon>mdi-plus</v-icon>
-                </v-btn>
             </v-list-item>
         </v-list>
 
@@ -41,35 +31,32 @@
             ></v-progress-linear>
 
             <!-- List -->
-            <v-list-item-group v-model="selectedUser">
-                <v-slide-y-transition group>
-                    <template v-for="user in friendList">
-                        <v-list-item
-                            :key="user.id"
-                            :value="user"
-                            v-if="!user._isMe"
-                            @click="onAddChat(user)"
-                        >
-                            <UserAvatar
-                                :user-name="me.fullName"
-                                :user="user"
-                                online-effect
-                            />
+            <v-slide-y-transition group>
+                <template v-for="user in friendList">
+                    <v-list-item
+                        :key="user.id"
+                        v-if="!user._isMe"
+                        @click="onAddChat(user)"
+                    >
+                        <UserAvatar
+                            :user-name="me.fullName"
+                            :user="user"
+                            online-effect
+                        />
 
-                            <v-list-item-content class="py-2 pl-3 pr-2">
-                                <v-list-item-title
-                                    class="body-2"
-                                    v-text="getDisplayName(user)"
-                                ></v-list-item-title>
-                                <v-list-item-subtitle
-                                    class="caption"
-                                    v-text="user.mail"
-                                ></v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </template>
-                </v-slide-y-transition>
-            </v-list-item-group>
+                        <v-list-item-content class="py-2 pl-3 pr-2">
+                            <v-list-item-title
+                                class="body-2"
+                                v-text="getDisplayName(user)"
+                            ></v-list-item-title>
+                            <v-list-item-subtitle
+                                class="caption"
+                                v-text="user.mail"
+                            ></v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-slide-y-transition>
         </v-list>
     </v-sheet>
 </template>
@@ -81,7 +68,6 @@ export default {
     components: { UserAvatar },
     data() {
         return {
-            selectedUser: null,
             loading: false,
             searchText: null,
             friendList: [],
@@ -104,11 +90,10 @@ export default {
     },
     methods: {
         onAddChat(selectedUser) {
-            const user = selectedUser || this.selectedUser;
             const existingChat = this.$store.state.chats.all.find(chat => {
                 if (chat.subscribers && chat.subscribers.length == 2) {
                     const matchedSub = chat.subscribers.filter(sub => {
-                        return sub._isMe == true || sub.id == user.id;
+                        return sub._isMe == true || sub.id == selectedUser.id;
                     });
 
                     return matchedSub.length == 2;
@@ -120,19 +105,13 @@ export default {
                 // Active exsiting chat
                 this.$store
                     .dispatch("chats/activeChat", existingChat.id)
-                    .then(() => {
-                        this.selectedUser = null;
-                    })
                     .catch(console.error);
                 return;
             }
 
             // Create new conversation first
             this.$store
-                .dispatch("chats/activeTmpChat", user)
-                .then(() => {
-                    this.selectedUser = null;
-                })
+                .dispatch("chats/activeTmpChat", selectedUser)
                 .catch(console.error);
         },
         searchFriend() {
@@ -178,25 +157,5 @@ export default {
 
 #friend-list >>> .v-text-field--rounded {
     border-radius: 20px;
-}
-
-#friend-list >>> .theme--light.v-btn {
-    color: #043752c7;
-}
-
-#friend-list >>> .theme--light.v-btn--outlined {
-    border: 1.5px solid #043752c7;
-}
-
-#friend-list >>> .theme--dark.v-btn--outlined {
-    border: 1.5px solid #ffffffca;
-}
-
-#friend-list >>> .theme--light.v-btn--outlined.v-btn--disabled {
-    border: 1.5px solid rgba(0, 0, 0, 0.26) !important;
-}
-
-#friend-list >>> .theme--dark.v-btn--outlined.v-btn--disabled {
-    border: 1.5px solid rgba(255, 255, 255, 0.3) !important;
 }
 </style>
