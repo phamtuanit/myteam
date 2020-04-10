94548483232a4d58b73899502fe9679aca85d380
tuanp <tuanp@hitachivantara.com>
2020-04-10 16:16:33 +0700

    Support multi-view

--- src/app/src/pages/main.vue
+++ src/app/src/pages/main.vue
@@ -2,16 +2,22 @@
     <v-content id="main-layout">
         <LeftDrawer></LeftDrawer>
         <v-container class="pa-0 fill-height" fluid>
-            <router-view key="app-content"></router-view>
+            <router-multi-view
+                class="multi-view-wrapper"
+                tag="div"
+            ></router-multi-view>
+            <!-- <keep-alive max="10">
+                <RouterView :key="$route.name"/>
+            </keep-alive> -->
         </v-container>
     </v-content>
 </template>
 
 <script>
 import LeftDrawer from "../components/left-drawer";
+import RouterView from "../components/router-view/multi-router-view.js";
 export default {
-    components: { LeftDrawer },
-    created() {},
+    components: { LeftDrawer, RouterView },
 };
 </script>
 
@@ -19,5 +25,15 @@ export default {
 #main-layout >>> .v-navigation-drawer .v-navigation-drawer__border {
     background-color: transparent !important;
     width: 0px;
+}
+
+.multi-view-wrapper {
+    width: 100%;
+    max-width: 100%;
+    height: 100vh;
+}
+
+.multi-view-wrapper > div:not([data-is-active]) {
+    display: none !important;
 }
 </style>
