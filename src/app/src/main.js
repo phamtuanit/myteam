bbc1e8d475a872b1a93029abc6ded10d0d7e6c99
tuanp <tuanp@hitachivantara.com>
2020-06-09 14:36:23 +0700

    Impl content menu

--- src/app/src/main.js
+++ src/app/src/main.js
@@ -15,14 +15,7 @@ const router = require("./plugins/vue-router").default(store);
 // Check electron environment
 window.isElectron = typeof process != "undefined";
 if (window.isElectron == true) {
-    const ipcRenderer = window.require("electron").ipcRenderer;
-    window.IoC.register("ipc", ipcRenderer);
-
-    window.app = {};
-    ipcRenderer.on("set-data", (sender, data) => {
-        window.app = data;
-    });
-    ipcRenderer.send("get-data");
+    setupElectonEnv();
 } else {
     const IPC = require("./plugins/empty-ipc-render.js");
     window.IoC.register("ipc", new IPC());
@@ -45,3 +38,44 @@ new Vue({
     },
     watch: {},
 }).$mount("#app");
+
+function setupElectonEnv() {
+    const electron = require("electron");
+    const ipcRenderer = electron.ipcRenderer;
+    window.IoC.register("ipc", ipcRenderer);
+
+    window.app = {};
+    ipcRenderer.on("set-data", (sender, data) => {
+        window.app = data;
+
+        // Setup content menu
+        const { Menu } = electron.remote;
+        const contextMenu = Menu.buildFromTemplate([
+            { label: "Cut", role: "cut" },
+            { label: "Copy", role: "copy" },
+            { label: "Paste", role: "paste" },
+            { label: "Select All", role: "selectall" },
+            { label: "Delete", role: "delete" },
+            { type: "separator" },
+            {
+                label: "App",
+                submenu: [
+                    { role: "reload" },
+                    // { role: "forcereload" },
+                    { type: "separator" },
+                    { role: "resetzoom" },
+                    { role: "zoomin" },
+                    { role: "zoomout" },
+                    { type: "separator" },
+                    { label: "Version: " + window.app.version },
+                ],
+            },
+        ]);
+
+        document.addEventListener("contextmenu", event => {
+            event.preventDefault();
+            contextMenu.popup();
+        });
+    });
+    ipcRenderer.send("get-data");
+}
