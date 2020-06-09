e4bf6f4e67c2facf04579bf87b4bf5c8e150aedb
tuanp <tuanp@hitachivantara.com>
2020-06-09 10:16:46 +0700

    Impl EmptyIpc

---
+++ src/app/src/plugins/empty-ipc-render.js
@@ -0,0 +1,14 @@
+module.exports = class IpcRender {
+    constructor() {
+        this.name = "EmptyIpc";
+    }
+    invoke() {}
+    on() {}
+    once() {}
+    removeAllListeners() {}
+    removeListener() {}
+    send() {}
+    sendSync() {}
+    sendTo() {}
+    sendToHost() {}
+};
