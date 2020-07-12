cfb062553e6d87600ef91ff7b17c28870e58fae5
tuanp <tuanp@hitachivantara.com>
2020-07-12 16:13:49 +0700

    Add extensions

---
+++ src/app/src/pages/apps/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="extension-content">
+        <router-view :key="$route.name"></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Extensions"
+};
+</script>
+
+<style>
+#extension-content {
+    height: 100%;
+    width: 100%;
+}
+</style>
