f3c065564ac3e7c0e673ff5ec3218dba59e016b0
tuanp <tuanp@hitachivantara.com>
2020-07-17 14:00:56 +0700

    Separate component

---
+++ src/app/src/pages/channel/pinned-message.vue
@@ -0,0 +1,143 @@
+<template>
+    <div class="message-item pinned-message-item">
+        <v-divider></v-divider>
+        <div class="d-flex flex-row py-2">
+            <UserAvatar :user="user" class="align-self-start mt-1" />
+            <div class="flex-grow-1 align-self-stretch ml-3">
+                <!-- Header -->
+                <div class="header center-y selection-disabled">
+                    <span class="user-name subtitle-2" v-text="userName"></span>
+                    <!-- Time -->
+                    <span class="caption" v-text="time"></span>
+                    <v-spacer></v-spacer>
+
+                    <!-- <v-btn
+                        fab
+                        class="mr-2"
+                        rounded
+                        icon
+                        height="26"
+                        width="26"
+                        title="Jump"
+                        @click="onJump"
+                    >
+                        <v-icon small class="action btn-jump"
+                            >mdi-ray-end-arrow</v-icon
+                        >
+                    </v-btn> -->
+                    <v-btn
+                        fab
+                        class="mr-0"
+                        rounded
+                        icon
+                        height="26"
+                        width="26"
+                        title="Unpin"
+                        @click="onUnpin()"
+                        v-if="message.pinnedByMe == true"
+                    >
+                        <v-icon class="action btn-unpin">mdi-minus</v-icon>
+                    </v-btn>
+                </div>
+
+                <!-- Content -->
+                <v-card-text
+                    class="pa-0 message-text hl"
+                    v-html="messageHtml"
+                ></v-card-text>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import UserAvatar from "../../components/avatar/user-avatar.vue";
+export default {
+    components: { UserAvatar },
+    props: {
+        message: {
+            type: Object,
+            default: () => ({}),
+        },
+    },
+    data() {
+        return {
+            user: {
+                fullName: "Unknown",
+            },
+            time: "",
+            userName: "",
+            messageHtml: "",
+        };
+    },
+    created() {
+        this.time = this.getDateTime();
+        this.messageHtml = this.getMessageHtml();
+        if (this.message.from.issuer) {
+            this.$store
+                .dispatch("users/resolve", this.message.from.issuer)
+                .then(user => {
+                    this.user = user;
+                    this.userName = this.getDisplayUserName();
+                })
+                .catch(console.error);
+        }
+    },
+    methods: {
+        onUnpin() {
+            this.$store
+                .dispatch("conversations/pinMessage", { message: this.message })
+                .catch(console.error);
+        },
+        onJump() {
+            this.$emit("jump", this.message);
+        },
+        getDisplayUserName() {
+            return (
+                this.user.fullName ||
+                this.user.firstName + ", " + this.user.lastName
+            );
+        },
+        getMessageHtml() {
+            return this.message.body.content;
+        },
+        getDateTime() {
+            const date = this.message.updated || this.message.created;
+            if (date) {
+                return new Date(date).toLocaleDateString();
+            }
+
+            return "";
+        },
+    },
+};
+</script>
+
+<style>
+.pinned-message-item >>> .message-text > p {
+    margin-bottom: 0;
+}
+
+.pinned-message-item .theme--light .message-text {
+    color: rgba(0, 0, 0, 0.8);
+}
+
+.pinned-message-item .theme--dark .message-text {
+    color: rgba(255, 255, 255, 0.7);
+}
+
+.pinned-message-item .user-name::after {
+    content: "\2022";
+    padding-left: 5px;
+    padding-right: 5px;
+    color: hsl(0, 0%, 72%);
+}
+
+.pinned-message-item .action {
+    opacity: 0.2;
+}
+
+.pinned-message-item:hover .action {
+    opacity: 1;
+}
+</style>
--- src/app/src/pages/channel/pinned-messages.vue
+++ src/app/src/pages/channel/pinned-messages.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="pinned-message fill-height d-flex flex-column">
+    <div class="pinned-messages fill-height d-flex flex-column">
         <!-- Search -->
-        <v-sheet class="pinned-message--title pa-0 center-y no-border-radius">
+        <v-sheet class="pinned-messages--title pa-0 center-y no-border-radius">
             <v-subheader class="ml-0 selection-disabled"
                 >Pinned messages
             </v-subheader>
@@ -28,80 +28,12 @@
         <v-list class="pinned-messages-list py-0 px-0">
             <div class="pinned-messages-content overflow-y-auto">
                 <template v-for="msg in messages">
-                    <div
+                    <PinnedMessage
                         :key="msg.id"
-                        class="px-4 message-item pinned-message-item"
-                        :value="msg.id"
+                        class="px-4"
+                        :message="msg"
                     >
-                        <v-divider></v-divider>
-                        <div class="d-flex flex-row py-2">
-                            <UserAvatar
-                                v-if="typeof msg.from.issuer === 'object'"
-                                :user-name="msg.from.issuer.fullName"
-                                :user="msg.from.issuer"
-                                class="align-self-start mt-1"
-                            />
-                            <div class="flex-grow-1 align-self-stretch ml-3">
-                                <!-- Header -->
-                                <div class="header center-y selection-disabled">
-                                    <span
-                                        class="user-name subtitle-2"
-                                        v-if="
-                                            typeof msg.from.issuer === 'object'
-                                        "
-                                        v-text="
-                                            getDisplayUserName(msg.from.issuer)
-                                        "
-                                    ></span>
-                                    <!-- Time -->
-                                    <span class="caption"
-                                        v-text="
-                                            getDateTime(
-                                                msg.created || msg.updated
-                                            )
-                                        "
-                                    ></span>
-                                    <v-spacer></v-spacer>
-
-                                    <!-- <v-btn
-                                        fab
-                                        class="mr-2"
-                                        rounded
-                                        icon
-                                        height="26"
-                                        width="26"
-                                        title="Jump"
-                                        @click="onJump"
-                                    >
-                                        <v-icon small class="action btn-jump"
-                                            >mdi-ray-end-arrow</v-icon
-                                        >
-                                    </v-btn> -->
-                                    <v-btn
-                                        fab
-                                        class="mr-1"
-                                        rounded
-                                        icon
-                                        height="26"
-                                        width="26"
-                                        title="Unpin"
-                                        @click="onUnpin(msg)"
-                                        v-if="msg.pinnedByMe == true"
-                                    >
-                                        <v-icon class="action btn-unpin"
-                                            >mdi-minus</v-icon
-                                        >
-                                    </v-btn>
-                                </div>
-
-                                <!-- Content -->
-                                <v-card-text
-                                    class="pa-0 message-text hl"
-                                    v-html="getMessageHtml(msg)"
-                                ></v-card-text>
-                            </div>
-                        </div>
-                    </div>
+                    </PinnedMessage>
                 </template>
             </div>
         </v-list>
@@ -110,10 +42,9 @@
 
 <script>
 import { fillHeight } from "../../utils/layout.js";
-import { mapState } from "vuex";
-import UserAvatar from "../../components/avatar/user-avatar.vue";
+import PinnedMessage from "./pinned-message";
 export default {
-    components: { UserAvatar },
+    components: { PinnedMessage },
     props: {
         conversation: {
             type: Object,
@@ -126,11 +57,6 @@ export default {
             messages: [],
         };
     },
-    computed: {
-        ...mapState({
-            cachedUsers: state => state.users.all,
-        }),
-    },
     created() {
         this.onRefresh();
     },
@@ -142,14 +68,6 @@ export default {
         this.conversation.pinnedMessages.splice(0);
     },
     methods: {
-        onUnpin(message) {
-            this.$store
-                .dispatch("conversations/pinMessage", { message })
-                .catch(console.error);
-        },
-        onJump(msg) {
-            this.$emit("jump", msg);
-        },
         onRefresh() {
             return this.$store
                 .dispatch("conversations/getPinnedMessage", {
@@ -160,68 +78,28 @@ export default {
                 })
                 .catch(console.error);
         },
-        getDisplayUserName(user) {
-            return user.fullName || user.firstName + ", " + user.lastName;
-        },
-        getMessageHtml(msg) {
-            return msg.body.content;
-        },
-        getDateTime(date) {
-            if (date) {
-                return new Date(date).toLocaleDateString();
-            }
-
-            return "";
-        },
     },
 };
 </script>
 
-<style scoped>
-.pinned-message {
+<style>
+.pinned-messages {
     max-width: 20vw;
-}
-
-.pinned-message .pinned-message--title {
-    min-width: 20vw;
 }
 
 .pinned-messages-list {
     height: 100%;
 }
 
-.pinned-messages-list >>> .message-text > p {
-    margin-bottom: 0;
+.pinned-messages .pinned-messages--title {
+    min-width: 20vw;
 }
 
-.theme--light .message-text {
-    color: rgba(0, 0, 0, 0.8);
-}
-
-.theme--dark .message-text {
-    color: rgba(255, 255, 255, 0.7);
-}
-
-.user-name::after {
-    content: "\2022";
-    padding-left: 5px;
-    padding-right: 5px;
-    color: hsl(0, 0%, 72%);
-}
-
-.btn-refresh {
+.pinned-messages--title .btn-refresh {
     opacity: 0.4;
 }
 
-.btn-refresh:hover {
-    opacity: 1;
-}
-
-.action {
-    opacity: 0.2;
-}
-
-.pinned-message-item:hover .action {
+.pinned-messages--title:hover .btn-refresh {
     opacity: 1;
 }
 </style>
