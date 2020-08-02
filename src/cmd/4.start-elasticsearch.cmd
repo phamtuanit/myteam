f9308da3e452a29f614a7c898bc72bca3cea6907
tuanp <tuanp@hitachivantara.com>
2020-08-02 15:35:52 +0700

    Update CMD

--- src/cmd/4.start-elasticsearch.cmd
+++ src/cmd/4.start-elasticsearch.cmd
@@ -3,7 +3,8 @@ cd /d %~dp0
 SET CURRPATH=%~dp0..\
 SET DB_PATH=%CURRPATH%DB
 SET ELASTIC_DB_PATH=%DB_PATH%\elasticsearch
+SET ELASTIC_DB_BK_PATH=%DB_PATH%\bk\elasticsearch
 SET ELASTIC_DB_LOG_PATH=%DB_PATH%\logs\elasticsearch
 
 echo Setting up elasticsearch: %ELASTIC_DB_PATH%
-elasticsearch -E network.host=0.0.0.0 -E cluster.name=my-team  -E node.name=node-248-1 -E cluster.initial_master_nodes=["node-248-1"] -E path.data=%ELASTIC_DB_PATH% -E path.logs=%ELASTIC_DB_LOG_PATH%
+elasticsearch -E network.host=0.0.0.0 -E cluster.name=my-team  -E node.name=node-248-1 -E path.repo=%ELASTIC_DB_BK_PATH% -E cluster.initial_master_nodes=["node-248-1"] -E path.data=%ELASTIC_DB_PATH% -E path.logs=%ELASTIC_DB_LOG_PATH%
--- src/cmd/start-all.sh
+++ src/cmd/start-all.sh
@@ -5,16 +5,17 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 CURRPATH=$(dirname "$SCRIPT")
 echo $CURRPATH
-DB_PATH=$CURRPATH/../DB
+DB_PATH=$CURRPATH/../db
 MONGO_DB_PATH=$DB_PATH/mongodb
-REDIS_DB_PATH=$DB_PATH/redis
-REDIS_DB_CONF=$REDIS_DB_PATH/redis.conf
-ELASTIC_DB_PATH=$DB_PATH/elasticsearch
-ELASTIC_DB_LOG_PATH=$DB_PATH/logs/elasticsearch
 
 start mongod.exe --dbpath=$MONGO_DB_PATH  --bind_ip_all
 
-start elasticsearch -E network.host=0.0.0.0 -E cluster.name=my-team  -E node.name=node-248-1 -E cluster.initial_master_nodes=["node-248-1"] -E path.data=$ELASTIC_DB_PATH -E path.logs=$ELASTIC_DB_LOG_PATH
+ELASTIC_DB_PATH=$DB_PATH/elasticsearch
+ELASTIC_DB_LOG_PATH=$DB_PATH/logs/elasticsearch
+ELASTIC_DB_BK_PATH=$DB_PATH/bk/elasticsearch
+start elasticsearch -E network.host=0.0.0.0 -E cluster.name=my-team  -E node.name=node-248-1 -E path.repo=$ELASTIC_DB_BK_PATH -E cluster.initial_master_nodes=["node-248-1"] -E path.data=$ELASTIC_DB_PATH -E path.logs=$ELASTIC_DB_LOG_PATH
 
+REDIS_DB_PATH=$DB_PATH/redis
+REDIS_DB_CONF=$REDIS_DB_PATH/redis.conf
 cd $REDIS_DB_PATH
 redis-server.exe $REDIS_DB_CONF
