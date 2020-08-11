ae9a2828107192d9e9dfa4b84ac5aaf511f703e6
tuanp <tuanp@hitachivantara.com>
2020-08-11 10:57:32 +0700

    Update Spinner CSS

--- src/app/src/components/infinity-scroll-top.vue
+++ src/app/src/components/infinity-scroll-top.vue
@@ -6,7 +6,7 @@
         >
             <div
                 v-show="isLoading & scrollBar"
-                class="spinner lds-dual-ring my-2"
+                class="spinner loading-spinner lds-dual-ring my-2"
             ></div>
             <v-btn
                 icon
@@ -155,35 +155,5 @@ export default {
     left: 0;
     right: 0;
     height: 4px;
-}
-
-.spinner {
-    width: 24px;
-    height: 24px;
-}
-
-.spinner.lds-dual-ring {
-    display: inline-block;
-}
-
-.spinner.lds-dual-ring:after {
-    content: " ";
-    display: block;
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-    border: 3px solid var(--primary-color);
-    border-color: var(--primary-color) transparent var(--primary-color)
-        transparent;
-    animation: lds-dual-ring 1.2s linear infinite;
-}
-
-@keyframes lds-dual-ring {
-    0% {
-        transform: rotate(0deg);
-    }
-    100% {
-        transform: rotate(360deg);
-    }
 }
 </style>
