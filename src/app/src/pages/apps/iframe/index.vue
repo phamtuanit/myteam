cab3611afb32990f4b3126202f28065eaad69af5
tuanp <tuana.pham@hitachivantara.com>
2020-08-26 13:21:34 +0700

    Impl Iframe

---
+++ src/app/src/pages/apps/iframe/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="iframe-wrapper">
+        <iframe :src="src" class="iframe" frameborder="0"></iframe>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Frame",
+    props: ['src'],
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style>
+.iframe-wrapper,
+.iframe {
+    height: 100%;
+    width: 100%;
+}
+</style>
