6f93f5e3a7a4e9279a6bf350a379bbf02ed29bb5
tuanp <tuana.pham@hitachivantara.com>
2020-11-06 09:50:06 +0700

    Improve infinity-scroll

--- src/app/src/components/infinity-scroll-top.vue
+++ src/app/src/components/infinity-scroll-top.vue
@@ -26,7 +26,6 @@ export default {
             isLoading: false,
             isReachedAllData: false,
             isReachedTop: false,
-            hasLoaded:false,
         };
     },
     created() {
@@ -44,25 +43,36 @@ export default {
             const scrollHeight = containerEl.scrollHeight;
 
             // Load more content
-            this.locker = this.load();
+            const result = this.load();
+            if (typeof result.then == "function") {
+                // Result is a promise
+                this.locker = result;
+            } else {
+                this.locker = Promise.resolve(result);
+            }
+
             this.locker.then(([list]) => {
                 // Append result
+                console.log(list);
                 this.isReachedAllData = !list || list.length == 0;
-                if (!this.isReachedAllData) {
-                    // If current data is not enought, need to load more
-                    setTimeout(() => {
-                        if (this.isReachedTop) {
-                            this.locker.finally(this.loadMore);
-                        }
-                    }, 300);
-                }
-
-                if (this.hasLoaded) {
-                    // Incase user scrolled up
-                    containerEl.scrollTop = -4 + containerEl.scrollHeight - scrollHeight;
+                // If current data is not enought, need to load more
+                setTimeout(() => {
+                    if (this.isReachedTop && !this.isReachedAllData) {
+                        this.locker.finally(this.loadMore);
+                    } else {
+                        // Incase loading continuously, scroll to botom
+                        scrollToBottom(containerEl);
+                    }
+                }, 1000);
+                
+                // Update scroll position
+                if (this.isReachedTop) {
+                    // Incase user is still at bottom
+                    const currContentHeight = containerEl.lastElementChild.getBoundingClientRect().top - containerEl.firstElementChild.getBoundingClientRect().top;
+                    containerEl.scrollTop = currContentHeight
                 } else {
-                    // Incase auto load to fill out viewable erea
-                    scrollToBottom(containerEl);
+                    // Incase user already scrolled to top
+                    containerEl.scrollTop = -4 + containerEl.scrollHeight - scrollHeight;
                 }
             }).finally(() => {
                 this.isLoading = false;
@@ -70,12 +80,6 @@ export default {
         },
         onReachingTopIntersect([evt]) {
             this.isReachedTop = evt.isIntersecting;
-            // Field 'hasLoaded' will be changed only one times after finishing the first load (or incase fill out viewable erea)
-            if (!this.hasLoaded) {
-                this.hasLoaded = !this.isReachedTop || this.isReachedAllData;
-            }
-
-            // If user reached to top
             if (evt.isIntersecting == true && this.isReachedAllData == false) {
                 this.locker.finally(this.loadMore);
             }
