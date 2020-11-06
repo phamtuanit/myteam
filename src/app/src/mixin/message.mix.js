387f2bdd3b1af8a00298f9fb23945fe3e58e105a
tuanp <tuana.pham@hitachivantara.com>
2020-11-06 17:44:56 +0700

    Improve time-ago

--- src/app/src/mixin/message.mix.js
+++ src/app/src/mixin/message.mix.js
@@ -8,22 +8,40 @@ export default {
     },
     data() {
         return {
-            timeAgo: null,
+            timeAgo: "",
         };
-    },
-    created() {
-        this.updatedUIMix();
     },
     mounted() {
         this.timer = setInterval(this.updatedUIMix, 60 * 1000);
+        this.updatedUIMix();
     },
     destroyed() {
         clearInterval(this.timer);
     },
     methods: {
         updatedUIMix() {
-            // TODO: will remove arrivalTime
-            this.timeAgo = toTimeAgo(this.message.created || this.message.arrivalTime);
+            const time = this.message.created || this.message.arrivalTime;
+            const date = new Date(time);
+            if (Date.now() - date.getTime() > (4 * 3600000)) { // 4 hours ago
+                clearTimeout(this.timer);
+                this.timeAgo = this.getSimpleTime(date);
+                return;
+            }
+            this.timeAgo = toTimeAgo(time);
+        },
+        getSimpleTime(date) {
+            if (date) {
+                const day = new Date(date);
+                return (
+                    day.toLocaleDateString() +
+                    " " +
+                    day.getHours() +
+                    ":" +
+                    day.getMinutes()
+                );
+            }
+
+            return "N/A";
         },
     },
 };
